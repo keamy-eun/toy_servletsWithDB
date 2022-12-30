c8446fe84b8217d7226d6ed406629c4427ca61bf
jhun1020 <jhun1020.kh@gmail.com>
2022-12-30 14:24:41 +0900

    dml replace

--- src/main/resources/ERD/toyServletDML.sql
+++ src/main/resources/ERD/toyServletDML.sql
@@ -12,3 +12,37 @@ VALUES ('U4', '티미', 'timy04', '11104', '010-5649-1597', '2000-05-30', 'timy0
 
 INSERT INTO USERDATA (USER_UID, NAME, ID, PASSWORD, PHONE_NUMBER, BIRTHDAY, EMAIL, SMS_AD, EMAIL_AD)
 VALUES ('U5', '죠니', 'jony05', '11105', '010-7533-1744', '1985-07-07', 'jony05@naver.com', true, false);
+
+-- -- -- QUESTION_LIST -- -- -- 
+INSERT INTO QUESTION_LIST(QUESTION_UID, QUESTION, ORDERS)
+VALUES ('Q1', '현재 사용중인 제품을 선택해주세요.', '1');
+
+INSERT INTO QUESTION_LIST(QUESTION_UID, QUESTION, ORDERS)
+VALUES ('Q2', '현재 사용중인 제품의 가격이 적당하다고 생각하십니까?', '2');
+
+INSERT INTO QUESTION_LIST(QUESTION_UID, QUESTION, ORDERS)
+VALUES ('Q3', '사용중인 제품의 음향은 만족스러우신가요?', '3');
+
+INSERT INTO QUESTION_LIST(QUESTION_UID, QUESTION, ORDERS)
+VALUES ('Q4', '사용중인 제품의 디자인은 만족스러우신가요?', '4');
+
+INSERT INTO QUESTION_LIST(QUESTION_UID, QUESTION, ORDERS)
+VALUES ('Q5', '사용중인 제품은 사용이 편리한가요?', '5');
+
+
+
+-- -- --  ANSWERLIST -- -- --
+
+INSERT INTO ANSWER_LIST(ANSWER_UID, ANSWER, ORDERS)
+VALUES ('A1', '전혀 아니다', '1'), ('A2', '아니다', '2'), ('A3', '보통이다', '3'), ('A4', '그렇다', '4'),
+('A5', '매우 그렇다', '5'),('A6', '무선이어폰', '6'),('A7', '헤드셋', '7'),('A8', '유선이어폰', '8');
+
+
+ -- -- --  QA_DATA -- -- -- 
+ 
+INSERT INTO QA_DATA(QUESTION_UID, ANSWER_UID)
+VALUES ('Q1', 'A6'),('Q1', 'A7'),('Q1', 'A8'),
+('Q2', 'A1'),('Q2', 'A2'),('Q2', 'A3'),('Q2', 'A4'),('Q2', 'A5'),
+('Q3', 'A1'),('Q3', 'A2'),('Q3', 'A3'),('Q3', 'A4'),('Q3', 'A5'),
+('Q4', 'A1'),('Q4', 'A2'),('Q4', 'A3'),('Q4', 'A4'),('Q4', 'A5'),
+('Q5', 'A1'),('Q5', 'A2'),('Q5', 'A3'),('Q5', 'A4'),('Q5', 'A5');
