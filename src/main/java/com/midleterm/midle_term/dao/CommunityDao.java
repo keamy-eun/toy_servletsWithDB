51fbb66409dff32e00e0c414b00af069db5da794
jhun1020 <jhun1020.kh@gmail.com>
2023-02-15 12:44:45 +0900

    sadf

--- src/main/java/com/midleterm/midle_term/dao/CommunityDao.java
+++ src/main/java/com/midleterm/midle_term/dao/CommunityDao.java
@@ -1,19 +1,19 @@
-package com.midleterm.midle_term.dao;
+// package com.midleterm.midle_term.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+// import org.mybatis.spring.SqlSessionTemplate;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class CommunityDao {
-    @Autowired
-    SqlSessionTemplate sqlSessionTemplate;
+// @Component
+// public class CommunityDao {
+//     @Autowired
+//     SqlSessionTemplate sqlSessionTemplate;
     
-    public Object 메소드명(String sqlMapId, Object dataMap){
-        Object result = sqlSessionTemplate.selectList(sqlMapId, dateMap);
-    }
+//     public Object 메소드명(String sqlMapId, Object dataMap){
+//         Object result = sqlSessionTemplate.selectList(sqlMapId, dateMap);
+//     }
     
-}
+// }
 
 
 
--- src/main/java/com/midleterm/midle_term/service/CommunityService.java
+++ src/main/java/com/midleterm/midle_term/service/CommunityService.java
@@ -1,20 +1,20 @@
-package com.midleterm.midle_term.service;
+// package com.midleterm.midle_term.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.midleterm.midle_term.dao.CommunityDao;
+// // import com.midleterm.midle_term.dao.CommunityDao;
 
-@Service
-public class CommunityService {
+// @Service
+// public class CommunityService {
 
-    @Autowired
-    CommunityDao communityDao;
+//     @Autowired
+//     CommunityDao communityDao;
 
 
-    public Object gerLecture(Object dataMap){
-        String sqlMapId = "매퍼이름 @@.@@@";
-        Object result = communityDao.메소드명(sqlMapId, dataMap);
-        return result;
-    }
-}
+//     public Object gerLecture(Object dataMap){
+//         String sqlMapId = "매퍼이름 @@.@@@";
+//         Object result = communityDao.메소드명(sqlMapId, dataMap);
+//         return result;
+//     }
+// }
